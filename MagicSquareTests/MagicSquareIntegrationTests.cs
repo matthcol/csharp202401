be45e18d40d6a845e88d63b9ac993b85d53ec295
matthcol <matthias.colin@gmail.com>
2024-01-03 18:47:14 +0100

    remove unused args

--- MagicSquareTests/MagicSquareIntegrationTests.cs
+++ MagicSquareTests/MagicSquareIntegrationTests.cs
@@ -24,16 +24,16 @@ namespace Magic
         {
             return new List<Object[]>()
             {
-                new object[] { SquareFixture.SQUARE_3_OK, 3, 15 },
-                new object[] { SquareFixture.SQUARE_4_OK_ALBRECHT_DURER, 4, 34 },
-                new object[] { SquareFixture.SQUARE_5_OK, 5, 65 },
-                new object[] { SquareFixture.SQUARE_12_OK_WILLEM_BARINK, 12, 870 }
+                new object[] {SquareFixture.SQUARE_3_OK},
+                new object[] {SquareFixture.SQUARE_4_OK_ALBRECHT_DURER},
+                new object[] {SquareFixture.SQUARE_5_OK},
+                new object[] {SquareFixture.SQUARE_12_OK_WILLEM_BARINK}
             };
         }
 
         [Theory]
         [MemberData(nameof(MagicSquaresOk))]
-        public void IsMagicTest_OK(UInt32[,] square, UInt32 n, UInt32 magicSum)
+        public void IsMagicTest_OK(UInt32[,] square)
         {
             var ok = MagicSquare.IsMagic(square);
             Assert.True(ok);
@@ -43,20 +43,20 @@ namespace Magic
         {
             return new List<Object[]>()
             {
-                new object[] { SquareFixture.SQUARE_3_KO_ROWS, 3, 15 },
-                new object[] { SquareFixture.SQUARE_3_KO_COLUMNS, 3, 15 },
-                new object[] { SquareFixture.SQUARE_3_KO_2_DIAGS, 3, 15 },
-                new object[] { SquareFixture.SQUARE_3_KO_DIAG1, 3, 15 },
-                new object[] { SquareFixture.SQUARE_3_KO_DIAG2, 3, 15 },
-                new object[] { SquareFixture.SQUARE_3_KO_OUT_OF_RANGE_UNDER, 3, 15 },
-                new object[] { SquareFixture.SQUARE_3_KO_OUT_OF_RANGE_ABOVE, 3, 15 },
-                new object[] { SquareFixture.SQUARE_3_KO_REPEATED_VALUE, 3, 15 },
+                new object[] { SquareFixture.SQUARE_3_KO_ROWS },
+                new object[] { SquareFixture.SQUARE_3_KO_COLUMNS },
+                new object[] { SquareFixture.SQUARE_3_KO_2_DIAGS },
+                new object[] { SquareFixture.SQUARE_3_KO_DIAG1 },
+                new object[] { SquareFixture.SQUARE_3_KO_DIAG2 },
+                new object[] { SquareFixture.SQUARE_3_KO_OUT_OF_RANGE_UNDER },
+                new object[] { SquareFixture.SQUARE_3_KO_OUT_OF_RANGE_ABOVE },
+                new object[] { SquareFixture.SQUARE_3_KO_REPEATED_VALUE },
             };
         }
 
         [Theory]
         [MemberData(nameof(MagicSquareRowsKo))]
-        public void IsMagicTest_KO(UInt32[,] square, UInt32 n, UInt32 magicSum)
+        public void IsMagicTest_KO(UInt32[,] square)
         {
             var ok = MagicSquare.IsMagic(square);
             Assert.False(ok);
